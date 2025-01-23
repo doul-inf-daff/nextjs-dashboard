a704e18df16af9af0dfc2d52f061b398e864e9a0
doulinf.daff91@gmail.com <douldaff@gmail.com>
2025-01-23 16:22:21 +0000

    add global.css, config fonts and image

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana, Roboto } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight:["400", "700"]
+  })
